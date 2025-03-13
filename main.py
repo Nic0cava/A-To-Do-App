6a4d5b1e8a11f858a133eb104e4dc76c48644194
Nico <nmcava01@gmail.com>
2025-03-12 22:07:16 -0400

    first draft and layout of todo app

---
+++ main.py
@@ -0,0 +1,45 @@
+# To-Do App
+
+
+
+while True: 
+    user_action = input("Type add, show, edit, complete, or exit: ")
+    user_action = user_action.strip()
+    user_action = user_action.lower()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+        case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+            
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            number = number - 1
+            todos.pop(number)
+        case 'exit':
+            break
+        case _:
+            print("Invalid action, try again.")
+
+print("Goodbye!")
